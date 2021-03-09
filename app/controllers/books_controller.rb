2f267215b1234744f56cc788abf554a4e7baee2f
sagarakoudai <magnons180@gmail.com>
2021-03-09 11:02:43 +0000

    add books_index_show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,36 @@
-class BooksController <
+class BooksController < ApplicationController
+
+  def new
+  end
 
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to books_path
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
   private
-
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
+
+
 
 end

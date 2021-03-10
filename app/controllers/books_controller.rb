2d35c576d91738b302def6953de0582494fed15f
sagarakoudai <magnons180@gmail.com>
2021-03-10 08:51:33 +0000

    add profile_image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,15 +5,19 @@ class BooksController < ApplicationController
 
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      render :index
+    end
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def show
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '78ebdd5f7d976ad022b932aebf4824423781dc1384ddf4c93cd7e7e61981a0f1e00a90fdddfca047eb6e49bd30c95d8bc2f0b097cdd94b1594366bdb987b1739'
+

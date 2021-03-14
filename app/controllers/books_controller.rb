5b66b53a0f218cf319fa025263b8c9e7a7ba24cf
sagarakoudai <magnons180@gmail.com>
2021-03-14 15:42:55 +0000

    fix error

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
-      render :index
+      render ("books/edit")
     end
   end
 

2479256a1dca8b65dd20824fc719d9d0f2eeab7b
sagarakoudai <magnons180@gmail.com>
2021-03-18 07:21:29 +0000

    fix migration_books

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_071354) do
+ActiveRecord::Schema.define(version: 2021_03_18_065759) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.string "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 2021_03_13_071354) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "introduction"
     t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
